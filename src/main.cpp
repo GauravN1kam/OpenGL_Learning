7138fd3497d3e87d54c3c4b944d0fca23c844b16
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-01-03 23:52:34 +0530

    Added triangle

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,21 @@ int main() {
   while (!glfwWindowShouldClose(window)) {
     glClearColor(0, 0, 0, 0);
     glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+
+    glColor3f(1.0f, 0.f, 0.f);
+    glVertex3f(-0.6f, -0.6f, 0);
+    glColor3f(0.0f, 1.f, 0.f);
+    glVertex3f(0.0f, 0.5f, 0);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.6f, -0.6f, 0);
+
+    glEnd();
+
+    glBegin(GL_LINES);
+    glVertex3f(-0.9, 0.8, 0);
+    glVertex3f(0.9, 0.8, 0);
+    glEnd();
 
     glfwSwapBuffers(window);
     glfwPollEvents();
