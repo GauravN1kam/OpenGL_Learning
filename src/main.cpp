af452b7a36f1b45220a4deaeee24f452d5ebca24
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-01-04 22:55:54 +0530

    Code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include "config.h"
+#include <cmath>
+
+void DrawCircle();
 
 int main() {
   GLFWwindow *window;
@@ -20,24 +23,46 @@ int main() {
   while (!glfwWindowShouldClose(window)) {
     glClearColor(0, 0, 0, 0);
     glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_TRIANGLES);
 
-    glColor3f(1.0f, 0.f, 0.f);
-    glVertex3f(-0.6f, -0.6f, 0);
-    glColor3f(0.0f, 1.f, 0.f);
-    glVertex3f(0.0f, 0.5f, 0);
-    glColor3f(0.f, 0.f, 1.f);
-    glVertex3f(0.6f, -0.6f, 0);
+    // glBegin(GL_TRIANGLES);
+    // glColor3f(1.0f, 0.f, 0.f);
+    // glVertex3f(-0.6f, -0.6f, 0);
+    // glColor3f(0.0f, 1.f, 0.f);
+    // glVertex3f(0.0f, 0.5f, 0);
+    // glColor3f(0.f, 0.f, 1.f);
+    // glVertex3f(0.6f, -0.6f, 0);
+    // glEnd();
 
-    glEnd();
-
-    glBegin(GL_LINES);
-    glVertex3f(-0.9, 0.8, 0);
-    glVertex3f(0.9, 0.8, 0);
-    glEnd();
-
+    DrawCircle();
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
   glfwTerminate();
+}
+
+void DrawCircle() {
+  const int steps = 120;
+  const float angle = 3.1415926 * 2.f / steps;
+
+  float xPos = 0.f;
+  float yPos = 0.f;
+  float radius = 0.5f;
+
+  float prevX = xPos;
+  float prevY = yPos - radius;
+
+  for (int i = 0; i <= steps; i++) {
+    float newX = radius * sin(angle * i);
+    float newY = -radius * cos(angle * i);
+
+    glColor3f(0.5f, 0.2f, 0.7f);
+    glBegin(GL_TRIANGLES);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(prevX, prevY, 0.f);
+    glVertex3f(newX, newY, 0);
+    glEnd();
+
+    prevX = newX;
+    prevY = newY;
+  }
 }
