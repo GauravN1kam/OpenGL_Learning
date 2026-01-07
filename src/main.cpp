d218640db0a8ef05144d2b044588f82aeff592ef
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-01-07 22:40:04 +0530

    Trying to create a sin wave

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "config.h"
+#include <OpenGL/gl.h>
+#include <cmath>
 
 int main() {
   GLFWwindow *window;
@@ -11,7 +13,20 @@ int main() {
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
-    glBegin(GL_TRIANGLES);
+    glPointSize(0.5f);
+    float xVal = 0.01;
+    float yVal = 0;
+    glColor3f(0.5f, 0.5f, 0.5f);
+    glPointSize(2.0f);
+    glBegin(GL_POINTS);
+
+    float x = 0.0f;
+    for (int i = 0; i < 1000; i++) {
+      float y = sin(x);
+      glVertex2f(x, y);
+      x += 0.01f;
+    }
+
     glEnd();
     glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
